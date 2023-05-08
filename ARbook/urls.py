32add8961c14ea5ccc01d90736d6661fe6fcbba6
Martin Lagarde <lagarde.martin@sfr.fr>
2023-05-08 19:08:55 +0200

    URLs changed

--- ARbook/urls.py
+++ ARbook/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('login', views.login_view, name='login'),
+    path('index', views.index, name='index'),
+    path('', views.login_view, name='login'),
     path('logout', views.logout_view, name='logout'),
     path('dashboard', views.dashboard, name='dashboard'),
 
